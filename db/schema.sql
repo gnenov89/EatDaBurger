85ed1802cedaabee83459624e34aaf6efedf4131
gnenov89 <georgi.nenov89@gmail.com>
2017-09-28 11:17:36 -0400

    foldersm added

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+CREATE TABLE burgers(
+id int(10) NOT NULL AUTO_INCREMENT,
+burger_name  VARCHAR(250) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
